0330597eceb58d798aff680893b02b537522070b
AdrianPayne <adriansacristan1993@gmail.com>
2024-06-23 21:46:47 +0100

    🔨 complete endpoint

--- src/main.py
+++ src/main.py
@@ -1,6 +1,19 @@
+import os
+import csv
 from fastapi import FastAPI, UploadFile, File, HTTPException
 from pydantic import BaseModel, Field
 from typing import Dict, Any
+
+from src.pdf_service import PdfService
+
+
+def read_database():
+    """ Read from mock database """
+    with open("data/database.csv", mode="r") as infile:
+        reader = csv.DictReader(infile)
+        data = {rows["Company Name"]: rows for rows in reader}
+    return data
+
 
 app = FastAPI()
 
@@ -35,6 +48,12 @@ class DataComparison(BaseModel):
 class ErrorResponse(BaseModel):
     detail: str = Field(..., example="Invalid file type")
 
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
 @app.post("/upload_pdf/", response_model=DataComparison, responses={400: {"model": ErrorResponse}, 404: {"model": ErrorResponse}})
 async def upload_pdf(file: UploadFile = File(...)):
     """
@@ -46,13 +65,41 @@ async def upload_pdf(file: UploadFile = File(...)):
     if file.content_type != "application/pdf":
         raise HTTPException(status_code=400, detail="Invalid file type")
     
+    # Write the PDF file
     file_location = f"assets/{file.filename}"
     with open(file_location, "wb") as f:
         f.write(file.file.read())
+
+    # Extract data from PDF using PdfService
+    pdf_service = PdfService(key="TEST_KEY")
+
+    try:
+        extracted_data = pdf_service.extract(file_path=file_location)
+    except FileNotFoundError:
+        raise HTTPException(status_code=404, detail="File not found")
+    
+
+    # Read from DB
+    database_data = read_database()
+    company_name = extracted_data["Company Name"]
+
+    if company_name not in database_data:
+        raise HTTPException(status_code=404, detail="Company not found in database")
+
+    db_data = database_data[company_name]
+
+    # Get discrepancies
+    discrepancies = {
+        key: {
+            "pdf": extracted_data[key],
+            "db": db_data.get(key)
+        }
+        for key in extracted_data if str(extracted_data[key]) != str(db_data.get(key))
+    }
     
     # Placeholder for further implementation
     return DataComparison(
-        extracted_data={},
-        database_data={},
-        discrepancies={}
+        extracted_data=extracted_data,
+        database_data=db_data,
+        discrepancies=discrepancies
     )
